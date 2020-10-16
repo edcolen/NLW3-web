c3347c1df91a04993d050fbf3ad15ad438174cea
Ed Colen <ed.colen@gmail.com>
2020-10-16 12:58:59 -0300

    first version

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import mapImageMarker from "../images/map-marker.svg";
-import { FiPlus } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+import { Map, TileLayer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+
+import mapImageMarker from "../images/map-marker.svg";
 
 import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
 	return (
-		<div className="page-map">
+		<div id="page-map">
 			<aside>
 				<header>
 					<img src={mapImageMarker} alt="Happy" />
@@ -20,8 +24,16 @@ function OrphanagesMap() {
 					<strong>São Paulo, SP</strong>
 				</footer>
 			</aside>
-			<div></div>
-			<Link to=" " className="createOrphanage">
+			<Map
+				center={[-23.5851547, -46.6465988]}
+				zoom={13}
+				style={{ width: "100%", height: "100%" }}
+			>
+				<TileLayer
+					url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+				/>
+			</Map>
+			<Link to=" " className="create-orphanage">
 				<FiPlus size={32} color="#FFF" />
 			</Link>
 		</div>
