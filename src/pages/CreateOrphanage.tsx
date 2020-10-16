ea9eb6bb569322ac0c3b544c36e2aa77dff53d2c
Ed Colen <ed.colen@gmail.com>
2020-10-16 19:42:45 -0300

    update create orphanage

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map, Marker, TileLayer } from "react-leaflet";
+import { LeafletMouseEvent } from "leaflet";
 
 import { FiPlus } from "react-icons/fi";
 
@@ -8,6 +9,21 @@ import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
 
 export default function CreateOrphanage() {
+	const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+	const [name, setName] = useState("");
+	const [about, setAbout] = useState("");
+	const [instructions, setInstructions] = useState("");
+	const [opening_hours, setOpeningHours] = useState("");
+
+	function handleMapClick(e: LeafletMouseEvent) {
+		const { lat, lng } = e.latlng;
+
+		setPosition({
+			latitude: lat,
+			longitude: lng,
+		});
+	}
+
 	return (
 		<div id="page-create-orphanage">
 			<Sidebar />
@@ -20,21 +36,28 @@ export default function CreateOrphanage() {
 							center={[-27.2092052, -49.6401092]}
 							style={{ width: "100%", height: 280 }}
 							zoom={15}
+							onClick={handleMapClick}
 						>
 							<TileLayer
 								url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
 							/>
 
-							<Marker
-								interactive={false}
-								icon={mapIcon}
-								position={[-27.2092052, -49.6401092]}
-							/>
+							{position.latitude !== 0 && (
+								<Marker
+									interactive={false}
+									icon={mapIcon}
+									position={[position.latitude, position.longitude]}
+								/>
+							)}
 						</Map>
 
 						<div className="input-block">
 							<label htmlFor="name">Nome</label>
-							<input id="name" />
+							<input
+								id="name"
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
 						</div>
 
 						<div className="input-block">
@@ -64,7 +87,7 @@ export default function CreateOrphanage() {
 						</div>
 
 						<div className="input-block">
-							<label htmlFor="opening_hours">Nome</label>
+							<label htmlFor="opening_hours">Horário de funcionamento</label>
 							<input id="opening_hours" />
 						</div>
 
